c8d612570d479cc0396dd1c0f5ab328aac6854f6
cweil42 <colinweil42@gmail.com>
2025-05-16 21:30:38 -0500

    CORS policy for FE

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
+app.use(cors({ origin: '*', credentials: true }));
+app.use(express.json());
+
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (_req, res) => {
